4ac304e969963a0c616086d52154b1bd342fa3e7
hazaifaali686 <hazaifaali686@gmail.com>
2024-05-31 12:42:56 +0500

    upload code

---
+++ src/data/BlogData.js
@@ -0,0 +1,69 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"How Algorithmic Trading Systems Work",
+        
+        date:"29 Sept, 2023",
+        imgSrc:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/blogpost/how-algorithmic-trading-systems-work/photo-1518186285589-2f7649de83e0.jpg",
+        link:"https://www.codewithharry.com/blogpost/how-algorithmic-trading-systems-work/"
+    },
+    {
+        id:2,
+        name:"Build an Email Validator with HTML, CSS, and JavaScript",
+        
+        date:"14 Sept, 2023",
+        imgSrc:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/blogpost/email-validator-using-html-css-js/without-css.png",
+        link:"https://www.codewithharry.com/blogpost/email-validator-using-html-css-js/"
+    },
+    {
+        id:3,
+        name:"How to Open the Terminal in Visual Studio Code",
+        
+        date:"23 August, 2023",
+        imgSrc:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/blogpost/how-to-open-the-terminal-in-visual-studio-code/veiw-terminal.png",
+        link:"https://www.codewithharry.com/blogpost/how-to-open-the-terminal-in-visual-studio-code/"
+    },
+    {
+        id:4,
+        name:"[Solved] ZeroDivisionError: division by zero",
+        
+        date:"16 August, 2023",
+        imgSrc:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/blogpost/solved-python-zerodivision-error/zero-division-error-input.png",
+        link:"https://www.codewithharry.com/blogpost/solved-python-zerodivision-error/"
+    },
+    {
+        id:5,
+        name:"Create a Currency Converter: A Step-by-Step Guide Using HTML, CSS, and JavaScript",
+        
+        date:"22 August, 2023",
+        imgSrc:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/blogpost/build-currency-converter-html-css-javascript/download-vs-code.png",
+        link:"https://www.codewithharry.com/blogpost/build-currency-converter-html-css-javascript/"
+    },
+    {
+        id:6,
+        name:"Python was not found; run without arguments to install from the microsoft store, or..",
+       
+        date:"5 Jan, 2021",
+        imgSrc:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/blogpost/solving-python-not-found/step-1-search-for-environment-variables.png",
+        link:"https://www.codewithharry.com/blogpost/solving-python-not-found-run-without-arguments/"
+
+    },
+   
+    // {
+    //     id:7,
+    //     name:"What is Redux ? Simply Explained!",
+        
+    //     date:"16 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    // },
+    // {
+    //     id:8,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+        
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,51 @@
+export const Work = [
+    {
+        id:1,
+        name:"Ella Responsive Website",
+        description:"It is build on HTML5 & CSS, with resposiveness .",
+        tags:["HTML5","CSS3"],
+        
+        demo:"https://hazaifaali686.github.io/ellamart/",
+        github:"https://github.com/hazaifaali686/ellamart"
+    },
+    {
+        id:2,
+        name:"Neoreach website",
+        description:"It is build with HTML and CSS and using animation for better experience",
+        tags:["HTML5","CSS3"],
+        
+        demo:"https://hazaifaali686.github.io/neoreach/",
+        github:"https://github.com/hazaifaali686/neoreach"
+        
+    },
+    {
+        id:3,
+        name:"Helion Page1",
+        description:"A clone of Helion website home page.",
+        tags:["HTML5",'CSS3'],
+        
+        demo:"https://hazaifaali686.github.io/helion1/",
+        github:"https://github.com/hazaifaali686/helion1"
+    },
+    {
+        id:4,
+        name:"Helion 2",
+        description:"A clone of Helion website personal page.",
+        tags:["HTML5",'CSS3'],
+        
+        demo:"https://hazaifaali686.github.io/helion3/",
+        github:"https://github.com/hazaifaali686/helion3"
+       
+    },{
+        id:5,
+        name:"Food Mart website",
+        description:"It is build with HTML5 , CSS3 and Javascript and ecommerece website and fully responsive",
+        tags:["HTML5","CSS3","Javascript"],
+        
+        demo:"https://hazaifaali686.github.io/foodmart/",
+        github:"https://github.com/hazaifaali686/foodmart"
+        
+    },
+    
+  
+] 
